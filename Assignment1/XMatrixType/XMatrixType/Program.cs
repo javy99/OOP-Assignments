a2286d265ae98cb54f0d090138b139f01b9cc8cd
Javlonbek Kosimov <68327261+javy99@users.noreply.github.com>
2023-04-19 20:21:07 +0500

    Add files via upload

---
+++ Assignment1/XMatrixType/XMatrixType/Program.cs
@@ -0,0 +1,242 @@
+﻿namespace XMatrixSolution
+{
+    internal class Program
+    {
+        static XMatrix matrix = null;
+        static XMatrix matrix2 = null;
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("=============== MENU ===============");
+                Console.WriteLine("1. Create matrix");
+                Console.WriteLine("2. Print matrix");
+                Console.WriteLine("3. Get entry");
+                Console.WriteLine("4. Set entry");
+                Console.WriteLine("5. Add matrices");
+                Console.WriteLine("6. Multiply matrices");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("====================================");
+                Console.Write("Choose: ");
+
+                int choice = int.Parse(Console.ReadLine());
+                Console.WriteLine("====================================");
+
+                switch (choice)
+                {
+                    case 1:
+                        CreateMatrix();
+                        break;
+                    case 2:
+                        PrintMatrix();
+                        break;
+                    case 3:
+                        GetEntry();
+                        break;
+                    case 4:
+                        SetEntry();
+                        break;
+                    case 5:
+                        AddMatrices();
+                        break;
+                    case 6:
+                        MultiplyMatrices();
+                        break;
+                    case 0:
+                        return;
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+
+        static void CreateMatrix()
+        {
+            bool ok = false;
+            int size = -1;
+            do
+            {
+                try
+                {
+                    Console.Write("Enter matrix size: ");
+                    size = int.Parse(Console.ReadLine());
+                    ok = size > 0;
+                    matrix = new XMatrix(size);
+                    Console.WriteLine("====================================");
+                }
+                catch (XMatrix.NegativeSizeException)
+                {
+                    Console.WriteLine("Size of matrix should be positive!");
+                }
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("Invalid Format!");
+                }
+            } while (!ok);
+        }
+
+        static void PrintMatrix()
+        {
+            if (matrix == null)
+            {
+                Console.WriteLine("Matrix not created!");
+                Console.WriteLine("====================================");
+            }
+            else
+            {
+                matrix.Print();
+                Console.WriteLine("====================================");
+            }
+        }
+
+        static void GetEntry()
+        {
+            if (matrix == null)
+            {
+                Console.WriteLine("Matrix is not created!");
+                Console.WriteLine("====================================");
+            }
+            else
+            {
+                try
+                {
+                    Console.Write("Enter row index: ");
+                    int row = int.Parse(Console.ReadLine());
+
+                    Console.Write("Enter column index: ");
+                    int col = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Entry value: " + matrix.GetEntry(row, col));
+                    Console.WriteLine("====================================");
+                }
+                catch (XMatrix.ReferenceToNullPartException)
+                {
+                    Console.WriteLine("Invalid index(es)!");
+                }
+
+            }
+        }
+
+        static void SetEntry()
+        {
+            if (matrix == null)
+            {
+                Console.WriteLine("Matrix is not created!");
+            }
+            else
+            {
+                try
+                {
+                    Console.Write("Enter row index: ");
+                    int row = int.Parse(Console.ReadLine());
+
+
+                    Console.Write("Enter column index: ");
+                    int col = int.Parse(Console.ReadLine());
+
+                    Console.Write("Enter entry value: ");
+                    int value = int.Parse(Console.ReadLine());
+                    matrix.SetEntry(row, col, value);
+                    Console.WriteLine("====================================");
+                }
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("Invalid index(es)!");
+                }
+                catch (XMatrix.ReferenceToNullPartException)
+                {
+                    Console.WriteLine("Only elements on the diagonal may be non-zero!");
+                }
+            }
+        }
+
+        static void AddMatrices()
+        {
+            if (matrix == null)
+            {
+                Console.WriteLine("First matrix is not created!");
+            }
+            else
+            {
+                CreateSecondMatrix();
+                Console.WriteLine("====================================");
+                try
+                {
+                    XMatrix result = XMatrix.Add(matrix, matrix2);
+                    result.Print();
+                    Console.WriteLine("====================================");
+                }
+                catch (XMatrix.DifferentSizeException)
+                {
+                    Console.WriteLine("Both matrices should be of the same size!");
+                }
+            }
+        }
+
+        static void MultiplyMatrices()
+        {
+            if (matrix == null)
+            {
+                Console.WriteLine("First matrix not created!");
+            }
+            else
+            {
+                CreateSecondMatrix();
+                Console.WriteLine("====================================");
+                try
+                {
+                    XMatrix result = XMatrix.Multiply(matrix, matrix2);
+                    result.Print();
+                    Console.WriteLine("====================================");
+                }
+                catch (XMatrix.DifferentSizeException)
+                {
+                    Console.WriteLine("Both matrices should be of the same size!");
+                }
+
+            }
+        }
+
+        static void CreateSecondMatrix()
+        {
+            bool ok2 = false;
+            int size2 = -1;
+            do
+            {
+                try
+                {
+                    Console.Write("Enter second matrix size: ");
+                    size2 = int.Parse(Console.ReadLine());
+                    ok2 = size2 > 0;
+                    matrix2 = new XMatrix(size2);
+                    Console.WriteLine("====================================");
+                }
+                catch (XMatrix.NegativeSizeException)
+                {
+                    Console.WriteLine("Size of matrix should be positive!");
+                }
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("Invalid Format!");
+                }
+            } while (!ok2);
+
+            for (int i = 0; i < size2; i++)
+            {
+                Console.Write($"Enter element at diagonal ({i}, {i}): ");
+                int mainDiagonalEntry = int.Parse(Console.ReadLine());
+                matrix2.SetEntry(i, i, mainDiagonalEntry);
+
+                if (i != size2 - 1 - i)
+                {
+                    Console.Write($"Enter element at diagonal ({i}, {size2 - 1 - i}): ");
+                    int secondaryDiagonalEntry = int.Parse(Console.ReadLine());
+                    matrix2.SetEntry(i, size2 - 1 - i, secondaryDiagonalEntry);
+                }
+            }
+        }
+    }
+}
---
+++ Assignment1/XMatrixType/XMatrixType/XMatrix.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMatrixSolution
+{
+    
+    public class XMatrix
+    {
+        #region Exceptions
+        public class NegativeSizeException : Exception { };
+        public class ReferenceToNullPartException : Exception { };
+        public class DifferentSizeException : Exception { };
+        #endregion
+
+        #region Attributes
+        private List<int> mainDiagonal;
+        private List<int> secondaryDiagonal;
+        private int size;
+        #endregion
+
+        #region Constructor
+        public XMatrix(int size)
+        {
+            if (size <= 0) throw new NegativeSizeException();
+           
+            this.size = size;
+            mainDiagonal = new List<int>(size);
+            secondaryDiagonal = new List<int>(size);
+
+            for (int i = 0; i < size; i++)
+            {
+                mainDiagonal.Add(0);
+                secondaryDiagonal.Add(0);
+            }
+        }
+        public XMatrix(XMatrix x)
+        {
+            mainDiagonal = new List<int>(x.mainDiagonal);
+            secondaryDiagonal = new List<int>(x.secondaryDiagonal);
+        }
+        #endregion
+
+
+        public int GetEntry(int i, int j)
+        {
+            if (i < 0 || i >= size || j < 0 || j >= size)
+            {
+                throw new ReferenceToNullPartException();
+            }
+
+            if (i == j)
+            {
+                return mainDiagonal[i];
+            }
+            else if (i + j == size - 1)
+            {
+                return secondaryDiagonal[i];
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public void SetEntry(int i, int j, int value)
+        {
+            if (i < 0 || i >= size || j < 0 || j >= size)
+            {
+                throw new FormatException();
+            }
+
+            if (i == j)
+            {
+                mainDiagonal[i] = value;
+            }
+            else if (i + j == size - 1)
+            {
+                secondaryDiagonal[i] = value;
+            }
+            else
+            {
+                if (value != 0)
+                {
+                    throw new ReferenceToNullPartException();
+                }
+
+            }
+        }
+
+        public static XMatrix Add(XMatrix a, XMatrix b)
+        {
+            if (a.size != b.size) throw new DifferentSizeException();
+           
+            XMatrix result = new XMatrix(a.size);
+
+            for (int i = 0; i < a.size; i++)
+            {
+                result.mainDiagonal[i] = a.mainDiagonal[i] + b.mainDiagonal[i];
+                result.secondaryDiagonal[i] = a.secondaryDiagonal[i] + b.secondaryDiagonal[i];
+            }
+
+            return result;
+        }
+
+        public static XMatrix Multiply(XMatrix a, XMatrix b)
+        {
+            
+            if (a.size != b.size)
+            {
+                throw new DifferentSizeException();
+            }
+            
+
+            XMatrix result = new XMatrix(a.size);
+
+            for (int i = 0; i < a.size; i++)
+            {
+                for (int j = 0; j < a.size; j++)
+                {
+                    int value = 0;
+
+                    for (int k = 0; k < a.size; k++)
+                    {
+                        value += a.GetEntry(i, k) * b.GetEntry(k, j);
+                    }
+
+                    result.SetEntry(i, j, value);
+                }
+            }
+
+            return result;
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write(GetEntry(i, j) + "\t");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+
+}
+
